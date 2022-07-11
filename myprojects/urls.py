60c9e04410e42f6c03676f28b7d3f33ec274beb7
Anton Boiko <bo1ko.ant@yandex.com>
2022-07-12 01:09:35 +0400

    Changed the default page

--- myprojects/urls.py
+++ myprojects/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('myprojects/', views.myprojects, name="projects"),
+    path('', views.myprojects, name="projects"),
     path('myproject/<str:pk>', views.myproject, name="projects"),
 ]
