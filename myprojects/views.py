cc7dff252052ea811009a63e88a75012aee36f7c
Anton Boiko <bo1ko.ant@yandex.com>
2022-07-12 10:25:54 +0400

    added tags for the main pages

--- myprojects/views.py
+++ myprojects/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def myprojects(request):
-    return render(request, 'projects/projects.html')
+    msg = 'Hello! You are on the project page!'
+    return render(request, 'projects/projects.html', {'message':msg})
 
 def myproject(request, pk):
     return render(request, 'projects/single-project.html')
