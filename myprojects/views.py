8941f4f3654c2cc2eef98cb0442c2f971a965169
Anton Boiko <bo1ko.ant@yandex.com>
2022-07-12 23:20:41 +0400

    working with dictionaries

--- myprojects/views.py
+++ myprojects/views.py
@@ -1,9 +1,35 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+projectsList = [
+    {
+     'id': '1',
+     'title': 'Ecommerce Website',
+     'description': 'Fully functional ecommerce website',   
+        
+    },
+    {
+     'id': '2',
+     'title': 'Portfolio Website',
+     'description': 'Personal Portfolio of the Developer',   
+        
+    },
+    {
+     'id': '3',
+     'title': 'eLearning Platform',
+     'description': 'A Platform for learning English',   
+        
+    },
+
+]
+
+
 def myprojects(request):
-    msg = 'Hello! You are on the project page!'
-    return render(request, 'projects/projects.html', {'message':msg})
+    page = 'myprojects'
+    number = 10
+    context = {'page': page, 'number': number, 'myprojects': projectsList}
+    return render(request, 'projects/projects.html', context)
+
 
 def myproject(request, pk):
     return render(request, 'projects/single-project.html')
