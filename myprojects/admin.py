6cf10ddae1bb52ca0b4f977ea29e7aae00329527
Anton Boiko <bo1ko.ant@yandex.com>
2022-07-16 12:53:23 +0400

    added Tag and Review classes and migrated them

--- myprojects/admin.py
+++ myprojects/admin.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import MyProject
+from .models import MyProject, Review, Tag
 
 
 admin.site.register(MyProject)
+admin.site.register(Review)
+admin.site.register(Tag)
--- myprojects/models.py
+++ myprojects/models.py
@@ -4,14 +4,43 @@ import uuid
 # Create your models here.
 
 
+class Tag(models.Model):
+    name = models.CharField(max_length=200)
+    created_value = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          editable=False, primary_key=True)
+
+    def __str__(self):
+        return self.name
+
+
 class MyProject (models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
     demo_link = models.CharField(max_length=200, null=True, blank=True)
     source_code = models.CharField(max_length=2000, null=True, blank=True)
+    tags = models.ManyToManyField('Tag', blank=True)
+    vote_total = models.IntegerField(default=0, null=True, blank=True)
     created_value = models.DateTimeField(auto_now_add=True)
-    id = models.UUIDField(default=uuid.uuid4, unique=True, editable=False, primary_key=True)
-    
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          editable=False, primary_key=True)
+
     def __str__(self):
         return self.title
-    
+
+
+class Review (models.Model):
+    VOTE_TYPE = (
+        ('up', 'Up Vote'),
+        ('down', 'Down Vote'),
+    )
+    # owner =
+    project = models.ForeignKey(MyProject, on_delete=models.CASCADE)
+    body = models.TextField(null=True, blank=True)
+    value = models.CharField(max_length=200, choices=VOTE_TYPE)
+    created_value = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          editable=False, primary_key=True)
+
+    def __str__(self):
+        return self.value
