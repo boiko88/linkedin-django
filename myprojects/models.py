9312523ddcb60b92447f3e71018ba513e832caab
Anton Boiko <bo1ko.ant@yandex.com>
2022-07-17 20:51:27 +0400

    added and migrated vote ratio

--- myprojects/models.py
+++ myprojects/models.py
@@ -19,8 +19,9 @@ class MyProject (models.Model):
     description = models.TextField(null=True, blank=True)
     demo_link = models.CharField(max_length=200, null=True, blank=True)
     source_code = models.CharField(max_length=2000, null=True, blank=True)
-    tags = models.ManyToManyField('Tag', blank=True)
+    tags = models.ManyToManyField(Tag, blank=True)
     vote_total = models.IntegerField(default=0, null=True, blank=True)
+    vote_ratio = models.IntegerField(default=0, null=True, blank=True)
     created_value = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(default=uuid.uuid4, unique=True,
                           editable=False, primary_key=True)
